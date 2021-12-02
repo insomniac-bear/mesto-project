67787c2573fd2dd483a9092efde376271fd0e0ac
K-Tcherapanov <tcherepanov.kir@gmail.com>
2021-12-02 23:56:49 +0700

    Добавил попап добавления новой карточки

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function closePopup (popupElement) {
+  popupElement.classList.remove('popup_opened');
+};
+
 /* 
 * Реализация функционала открытия и закрытия попапа
 * редактирования профиля
@@ -9,31 +13,30 @@ const profileName = profile.querySelector('.profile__name');
 const profileProfession = profile.querySelector('.profile__profession');
 const editProfileButton = profile.querySelector('.profile__button-edit');
 // Функция открытия попапа редактирования профиля
-function openPopup () {
+function openEditProfilePopup () {
   editProfilePopup.classList.add('popup_opened');
   // Выбираем управляющие элементы попапа
-  const closeProfileButton = editProfilePopup.querySelector('.popup__close-button');
+  const closeProfileButton = editProfilePopup.querySelector('.popup__close-button_place_edit-profile');
   const editProfileForm = editProfilePopup.querySelector('.form_type_edit-profile');
   const nameInput = editProfilePopup.querySelector('.form__item_el_name');
   const professionInput = editProfilePopup.querySelector('.form__item_el_profession');
-  function closePopup () {
-    editProfilePopup.classList.remove('popup_opened');
-  };
+  // Функция обработки отправки формы
   function editProfileFormHandler (evt) {
     evt.preventDefault();
-    const name = nameInput.value;
-    const profession = professionInput.value;
-    profileName.textContent = name;
-    profileProfession.textContent = profession;
-    closePopup();
+    profileName.textContent = nameInput.value;
+    profileProfession.textContent = professionInput.value;
+    closePopup(editProfilePopup);
   };
   // Устанавливаем значение полей форм
   nameInput.value = profileName.textContent;
   professionInput.value = profileProfession.textContent;
+  // Устанавливаем обработчики событий на элементы управления попапа
   editProfileForm.addEventListener('submit', editProfileFormHandler, {once: true});
-  closeProfileButton.addEventListener('click', closePopup, {once: true});
+  closeProfileButton.addEventListener('click', function () {
+    closePopup(editProfilePopup)
+  }, {once: true});
 };
-editProfileButton.addEventListener('click', openPopup);
+editProfileButton.addEventListener('click', openEditProfilePopup);
 
 /**
  * Реализация функционала наполнения страницы стартовыми
@@ -66,7 +69,6 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-
 function renderCard (imageLink, imageName) {
   return (`
     <article class="photo">
@@ -78,7 +80,26 @@ function renderCard (imageLink, imageName) {
     </article>
   `);
 }
-
 for (let i = 0; i < initialCards.length; i++) {
   photosContainer.insertAdjacentHTML('beforeend', renderCard(initialCards[i].link, initialCards[i].name));
 }
+/* 
+* Реализация функционала открытия и закрытия попапа
+* добавления новой карточки
+*/
+const addNewCardButton = profile.querySelector('.profile__button-add');
+const addNewCardPopup = document.querySelector('.popup_type_add-card');
+// Функция открытия попапа редактирования профиля
+function openAddNewCardPopup () {
+  // Выбираем управляющие элементы попапа
+  const closeAddCardPopupButton = addNewCardPopup.querySelector('.popup__close-button_place_add-card');
+  // Устанавливаем обработчики событий на элементы управления попапа
+  // editProfileForm.addEventListener('submit', editProfileFormHandler, {once: true});
+  closeAddCardPopupButton.addEventListener('click', function () {
+    closePopup(addNewCardPopup)
+  }, {once: true});
+
+  addNewCardPopup.classList.add('popup_opened');
+};
+
+addNewCardButton.addEventListener('click', openAddNewCardPopup);
